6059d764801f305d3d1cb47b332a4b072846f68f
DariMasu <masu0002@hz.nl>
2026-02-03 13:12:02 +0100

    created a Response class

--- src/Response.php
+++ src/Response.php
@@ -4,5 +4,24 @@ namespace Framework;
 
 class Response
 {
+    public int $responseCode;
 
+    public string $body;
+
+    public ?string $header;
+
+    public function __construct(string $body, int $responseCode = 200, ?string $header = null)
+    {
+        $this->responseCode = $responseCode;
+        $this->body = $body;
+        $this->header = $header;
+    }
+
+    public function echo(): void
+    {
+        if ($this->header) {
+            header($this->header);
+        }
+        echo $this->body;
+    }
 }
