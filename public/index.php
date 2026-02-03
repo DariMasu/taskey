5fc11d5bb3e152ae6eec2fe0bc1c37f818b1deba
DariMasu <masu0002@hz.nl>
2026-02-03 13:15:24 +0100

    added basic logic to manage request processing process

--- public/index.php
+++ public/index.php
@@ -1,2 +1,20 @@
 <?php
-echo "Hello World!";
+
+require __DIR__ . "/../vendor/autoload.php";
+
+use Framework\Kernel;
+use Framework\Request;
+
+$kernel = new Kernel();
+
+// extract path from the uri
+$urlPath = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+if (!is_string($urlPath)) {
+    $urlPath = '/';
+}
+
+$request = new Request($_SERVER['REQUEST_METHOD'], $urlPath, $_GET, $_POST);
+
+$response = $kernel->handle($request);
+
+$response->echo();
