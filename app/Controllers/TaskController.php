7b630dc29ea6b64cd1e48786de26b09167c8f465
DariMasu <masu0002@hz.nl>
2026-02-05 15:56:02 +0100

    created task controller with tasks and tasks/create methods

--- app/Controllers/TaskController.php
+++ app/Controllers/TaskController.php
@@ -2,7 +2,17 @@
 
 namespace App\Controllers;
 
+use Framework\Response;
+
 class TaskController
 {
+    public function index(): Response
+    {
+        return new Response('listing all tasks');
+    }
 
+    public function create(): Response
+    {
+        return new Response('creating a task');
+    }
 }
