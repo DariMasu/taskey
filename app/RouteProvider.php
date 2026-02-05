b6c1fd971460a0c6f86fa8cb4cbef8d832b652f1
DariMasu <masu0002@hz.nl>
2026-02-05 13:48:13 +0100

    cleaned up index.php - decoupled router/routes

---
+++ app/RouteProvider.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Framework\RouteProviderInterface;
+use Framework\Router;
+
+class RouteProvider implements RouteProviderInterface
+{
+
+    public function register(Router $router): void
+    {
+        // TODO: Implement register() method.
+    }
+}
--- public/index.php
+++ public/index.php
@@ -4,8 +4,13 @@ require __DIR__ . "/../vendor/autoload.php";
 
 use Framework\Kernel;
 use Framework\Request;
+use App\RouteProvider;
 
+// registering routes
+$routeProvider = new RouteProvider();
 $kernel = new Kernel();
+$kernel->registerRoutes($routeProvider);
+
 
 // extract path from the uri
 $urlPath = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
@@ -13,6 +18,7 @@ if (!is_string($urlPath)) {
     $urlPath = '/';
 }
 
+// getting a new request
 $request = new Request($_SERVER['REQUEST_METHOD'], $urlPath, $_GET, $_POST);
 
 $response = $kernel->handle($request);
