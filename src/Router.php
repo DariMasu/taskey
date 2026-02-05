270f391823f7bcd5a651c0a83e0740e458f076ea
DariMasu <masu0002@hz.nl>
2026-02-05 15:44:06 +0100

    created a router class

--- src/Router.php
+++ src/Router.php
@@ -4,5 +4,36 @@ namespace Framework;
 
 class Router
 {
+    /** @var Route[] */
+    public array $routes;
 
+    public function __construct()
+    {
+    }
+
+    public function dispatch(Request $request): Response
+    {
+        $matchedRoute = null;
+
+        foreach ($this->routes as $route) {
+            if ($route->matches($request->method, $request->path)) {
+                $matchedRoute = $route;
+                break;
+            }
+        }
+
+        if ($matchedRoute === null) {
+            return new Response('Page not found :(');
+        } else {
+            $callback = $matchedRoute->callback;
+            $response = $callback();
+
+            return $response;
+        }
+    }
+
+    public function addRoute(string $method, string $path, callable $callback): void
+    {
+        $this->routes[] = new Route($method, $path, $callback);
+    }
 }
