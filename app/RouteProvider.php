32191af5b58065c3acf735ab04668fb6fea0f45c
DariMasu <masu0002@hz.nl>
2026-02-05 15:54:38 +0100

    implemented route provider interface

--- app/RouteProvider.php
+++ app/RouteProvider.php
@@ -2,14 +2,23 @@
 
 namespace App;
 
+use App\Controllers\HomeController;
+use App\Controllers\TaskController;
 use Framework\RouteProviderInterface;
 use Framework\Router;
 
 class RouteProvider implements RouteProviderInterface
 {
-
     public function register(Router $router): void
     {
-        // TODO: Implement register() method.
+        // creating and adding routes for home controller
+        $homeController = new HomeController();
+        $router->addRoute('GET', '/', [$homeController, 'index']);
+        $router->addRoute('GET', '/about', [$homeController, 'about']);
+
+        // creating and adding routes for task controller
+        $taskController = new TaskController();
+        $router->addRoute('GET', '/tasks', [$taskController, 'index']);
+        $router->addRoute('GET', '/tasks/create', [$taskController, 'create']);
     }
 }
