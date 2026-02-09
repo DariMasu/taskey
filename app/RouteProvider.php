74bb06582180fcc474071063bdf59ba79c4953c4
DariMasu <masu0002@hz.nl>
2026-02-09 20:10:40 +0100

    changed register method to make use of service container class, removed redundant instantiation of controllers

--- app/RouteProvider.php
+++ app/RouteProvider.php
@@ -6,18 +6,19 @@ use App\Controllers\HomeController;
 use App\Controllers\TaskController;
 use Framework\RouteProviderInterface;
 use Framework\Router;
+use Framework\ServiceContainer;
 
 class RouteProvider implements RouteProviderInterface
 {
-    public function register(Router $router): void
+    public function register(Router $router, ServiceContainer $container): void
     {
         // creating and adding routes for home controller
-        $homeController = new HomeController();
+        $homeController = $container->get(HomeController::class);
         $router->addRoute('GET', '/', [$homeController, 'index']);
         $router->addRoute('GET', '/about', [$homeController, 'about']);
 
         // creating and adding routes for task controller
-        $taskController = new TaskController();
+        $taskController = $container->get(TaskController::class);
         $router->addRoute('GET', '/tasks', [$taskController, 'index']);
         $router->addRoute('GET', '/tasks/create', [$taskController, 'create']);
     }
